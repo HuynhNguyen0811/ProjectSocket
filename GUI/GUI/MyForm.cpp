70842297039ef021a2b974c2d86e1c0e847661df
anhthu872 <20127343@student.hcmus.edu.vn>
2021-08-02 21:17:35 +0700

    GUI
    GUI

---
+++ GUI/GUI/MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+//#include<Windows.h>
+//using namespace System;
+//using namespace System::Windows::Forms;
+//
+//int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hPrecInst, LPSTR args , int ncmdshow) {
+//	GUI::Application::EnableVisualStyles();
+//	GUI::Application::SetCompatibleTextRenderingDefault(false);
+//	GUI::Application::Run(gcnew GUI::MyForm());
+//	return 0;
+//}
